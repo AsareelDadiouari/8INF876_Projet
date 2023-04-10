22478ceef29665806127bd84a1483c342736f8df
Coco Biturbo <74254008+cocoBiturbo@users.noreply.github.com>
2023-04-10 18:54:23 -0400

    [Feat] Add add-ticket component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import {RouterModule, RouterOutlet} from "@angular/router";
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AddTicketPageComponent } from './components/tickets/add-tickets.component';
 import {AuthModule} from "./components/auth/auth.module";
 import {AuthGuard} from "./auth.guard";
 import {HttpClientModule} from "@angular/common/http";
@@ -14,7 +15,8 @@ import {AppRoutingModule} from "./app.routing.module";
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    AddTicketPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/components/tickets/add-tickets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-page',
+  templateUrl: './add-tickets.component.html',
+  styleUrls: ['./add-tickets.component.css']
+})
+export class AddTicketPageComponent {
+
+}
